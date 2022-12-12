cd5d6da637f99b02654b96a89ff4b9cb49387b30
Timmy Elf <Timmy.Elf@yh.nackademin.se>
2022-12-12 08:52:21 +0100

    Test

--- test.py
+++ test.py
@@ -1,8 +1,30 @@
 import random
 
 
-treasure_list = ["Loose_change", "Money_bag", "Gold_jewelry", "Gemstone", "Small_treasure_chest"]
-antal_skatter = random.randint(0, 5)
-print(antal_skatter)
-random_treasure = random.choices(treasure_list, weights=(40, 20, 15, 10, 5), k=antal_skatter)
-print(random_treasure)
+#treasure_list = ["Loose_change", "Money_bag", "Gold_jewelry", "Gemstone", "Small_treasure_chest"]
+#antal_skatter = random.randint(0, 5)
+#print(antal_skatter)
+#random_treasure = random.choices(treasure_list, weights=(40, 20, 15, 10, 5), k=5)
+#print(random_treasure)
+
+random.randint(0, 100)
+if random.randint(0, 100) <= 40:
+    print("Loose_change")
+
+random.randint(0, 100)
+if random.randint(0, 100) <= 20:
+    print("Money_bag")
+
+random.randint(0, 100)
+if random.randint(0, 100) <= 15:
+    print("Gold_jewelry")
+
+random.randint(0, 100)
+if random.randint(0, 100) <= 10:
+    print("Gemstone")
+
+random.randint(0, 100)
+if random.randint(0, 100) <= 5:
+    print("Small_treasure_chest")
+
+# Istället för print kan man lägga till spelkaraktären i en lista och sedan skriva ut listan.
