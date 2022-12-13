0e90ba91645aef6ad5807fbf01a00efda1c26221
Mario Khalaf <Mario.khalaf9810@gmail.com>
2022-12-13 04:00:01 +0100

    rooms created & corners

--- Dungeon_run.py
+++ Dungeon_run.py
@@ -26,102 +26,126 @@ class Player:
             if value["Name"] == hero_name:
                 return True
         return False
+    
+    
+    def load_existing_account(self):
+        account_name = input("Name of your saved account: ").capitalize()
+        with open("saved_games.json") as f:
+            data = json.load(f)
+        for i, player in enumerate(data["Players"]):
+            if player["Name"] == account_name:  # if name of account is in there, return True
+                print(f"\nWelcome back {account_name}")
+                return data["Players"][i]
+            else:
+                print("This account does not exist\n")
 
-
-def hero_choice(choose_hero):
-    heroes = [
-        {
-            "Hero": "Knight",
-            "Iniative": 5,
-            "Endurance": 9,
-            "Attack": 6,
-            "Flexibility": 4
-        },
-        {
-            "Hero": "Wizard",
-            "Iniative": 6,
-            "Endurance": 4,
-            "Attack": 9,
-            "Flexibility": 5
-        },
-        {
-            "Hero": "Thief",
-            "Iniative": 7,
-            "Endurance": 5,
-            "Attack": 5,
-            "Flexibility": 7
-        }
-    ]
-
-    if choose_hero == "1":
-        return heroes[0]
-
-    elif choose_hero == "2":
-        return heroes[1]
-
-    elif choose_hero == "3":
-        return heroes[2]
-
-
-def rooms():
-    size = int(input("Choose a map size by entering one of these numbers: 4,5,8: "))
-
-    for i in range(size):
-        for i in range(size):
-            print('X ', end='')
-        print()
-
-
-def room_menu():
-    rooms()
-    print("\n1. corner 1\n2. corner 2\n3. corner 3\n4. corner 4\nQ. Exit\n")
-    choice = input('Your choice: ').lower()
-    if choice == "1":
-        pass
-    print(f"\nYou will begin the game at corner {choice}")
-
-
-def load_existing_account():
-    account_name = input("Name of your saved account: ").capitalize()
-    with open("saved_games.json") as f:
-        data = json.load(f)
-    for i, player in enumerate(data["Players"]):
-        if player["Name"] == account_name:  # if name of account is in there, return True
-            print(f"\nWelcome back {account_name}")
-            return data["Players"][i]
+    def name_your_hero(self, hero):
+        hero_name = input("Choose a name for your hero: ").capitalize()
+        if self.check_name(hero_name):
+            print("This name already exists.")
         else:
-            print("This account does not exist\n")
+            character = hero["Hero"]
+            print(f"\nHello {hero_name}, You have chosen the {character} ")
+            for key, value in hero.items():
+                print(f"{key}, {value}")
+            self.write_to_json(hero_name, hero)
 
+    def hero_choice(self, choose_hero):
+        heroes = [
+            {
+                "Hero": "Knight",
+                "Iniative": 5,
+                "Endurance": 9,
+                "Attack": 6,
+                "Flexibility": 4 
+            },
+            {
+                "Hero": "Wizard",
+                "Iniative": 6,
+                "Endurance": 4,
+                "Attack": 9,
+                "Flexibility": 5
+            },
+            {
+                "Hero": "Thief",
+                "Iniative": 7,
+                "Endurance": 5,
+                "Attack": 5,
+                "Flexibility": 7
+            }
+        ]
 
-p = Player()
+        if choose_hero == "1":
+            return heroes[0]
 
+        elif choose_hero == "2":
+            return heroes[1]
 
-def name_your_hero(hero):
-    hero_name = input("Choose a name for your hero: ").capitalize()
-    if p.check_name(hero_name):
-        print("This name already exists.")
-    else:
-        character = hero["Hero"]
-        print(f"\nHello {hero_name}, You have chosen the {character} ")
-        for key, value in hero.items():
-            print(f"{key}, {value}")
-        p.write_to_json(hero_name, hero)
-        room_menu()
+        elif choose_hero == "3":
+            return heroes[2]
 
+class GameMap:
+    def __init__(self, size) -> None:
+        self.size = size
+        self.map = [["[ ]" for _ in range(self.size)] for _ in range(self.size)]
+
+    def room_menu(self):
+        for i in self.map:
+            print(' '.join(i))
+
+        print("\n1. corner 1\n2. corner 2\n3. corner 3\n4. corner 4\nQ. Exit\n")
+        choice = input('Your choice: ').lower()
+        print(f"\nYou will begin the game at corner {choice}\n")
+
+        if choice == "1":
+            self.map[0][0] = "[X]"
+            return  self.map
+        elif choice == "2":
+            self.map[0][self.size-1] = "[X]"
+            return self.map
+        elif choice == "3":
+            self.map[self.size-1][0] = "[X]"
+            return self.map
+        elif choice == "4":
+            self.map[self.size-1][self.size-1] = "[X]"
+            return self.map
+
+def game_menu(game_room):
+    while True:
+        for i in game_room:
+            print(' '.join(i))
+        option = input("\nChoose where to go\n\n1. Up\n2. Down\n3. Right\n4. Left")
+        if option == "1":
+            pass
+        if option == "1":
+            pass
+        if option == "1":
+            pass
+        if option == "1":
+            pass
 
 def main_menu():
+    p = Player()
     print("Welcome to the dungeon run!\nChoose your option and begin the adventure.\n")
     while True:
         option = input("1. Create a new hero\n2. Load existing hero\n3. Exit\n")
 
         if option == "1":
             choose_hero = input('Choose your hero.\n1. The knight\n2. The Wizard\n3. The Thief\n')
-            hero = hero_choice(choose_hero)
-            name_your_hero(hero)
+            hero = p.hero_choice(choose_hero)
+            p.name_your_hero(hero)
+            size = int(input("\nChoose a map size by entering one of these numbers: 4,5,8: "))
+            g = GameMap(size)
+            game_room = g.room_menu()
+            game_menu(game_room)
 
         elif option == "2":
-            account = load_existing_account()
+            account = p.load_existing_account()
             print(account)
+            size = int(input("\nChoose a map size by entering one of these numbers: 4,5,8: "))
+            g = GameMap(size)
+            g.room_menu()
+
         elif option == "3":
             exit("Goodbye!")
         else:
