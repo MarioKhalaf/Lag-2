6a5c7ebf6f8dc978cfee76c026f1f2f057751dbb
davidhasenson <90691386+davidhasenson@users.noreply.github.com>
2022-12-13 11:28:50 +0100

    mer

--- program/treasure.py
+++ program/treasure.py
@@ -10,6 +10,16 @@ class Treasure:
     def __str__(self):
         return self.name
 
+    def random_treasure():
+        treasure_list = [LooseChange(), MoneyBag(), GoldJewelry(), Gemstone(), SmallTreasureChest()]
+        random_treasures = []
+        treasure_value = 0
+        for treasure in treasure_list:
+            if random.randint(0, 100) <= treasure.probability:
+                random_treasures.append(treasure.name)
+                treasure_value += treasure.value
+        return (random_treasures, treasure_value)
+
 
 class LooseChange(Treasure):
 
@@ -39,16 +49,3 @@ class SmallTreasureChest(Treasure):
 
     def __init__(self):
         super().__init__("Small treasure chest", 20, 5)
-
-
-def random_treasure():
-    treasure_list = [LooseChange(), MoneyBag(), GoldJewelry(), Gemstone(), SmallTreasureChest()]
-    random_treasures = []
-    treasure_value = 0
-    for treasure in treasure_list:
-        if random.randint(0, 100) <= treasure.probability:
-            random_treasures.append(treasure.name)
-            treasure_value += treasure.value
-
-    print(random_treasures)
-    print(treasure_value)
